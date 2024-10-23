72f12a3166e36673f36e675adeb37b173831757b
OmarEshiet <157065542+OmarEshiet@users.noreply.github.com>
2024-10-23 09:47:50 -0400

    Update Contents.swift

--- student_Xcode15/Unit1/Lesson1_2/Lab_Introduction.playground/Pages/1. Exercise - Use Playgrounds.xcplaygroundpage/Contents.swift
+++ student_Xcode15/Unit1/Lesson1_2/Lab_Introduction.playground/Pages/1. Exercise - Use Playgrounds.xcplaygroundpage/Contents.swift
@@ -11,7 +11,7 @@ print("How to use playgrounds to make writing Swift fun and simple")
  Now print your own phrases to the console. Pick one of your favorite songs. Use your knowledge of the `print` function to display the song title and artist.
  */
 print("I like many types of music, but I mostly listen to rap. One of my favorite songs is Wats Wrong By Isaiah Rashid ft Kendrick Lamar")
-print("hiiiiiii")
+
 
 /*:
  Use multiple `print` functions to write out some of the lyrics to the song.
