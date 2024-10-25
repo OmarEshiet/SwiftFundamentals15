71c740337d25aa0896ab92c9e14da2306432253b
OmarEshiet <157065542+OmarEshiet@users.noreply.github.com>
2024-10-25 15:43:32 -0400

    Update Contents.swift

--- student_Xcode15/Unit1/Lesson1_4/Lab_Operators.playground/Pages/5. Exercise - Order of Operations.xcplaygroundpage/Contents.swift
+++ student_Xcode15/Unit1/Lesson1_4/Lab_Operators.playground/Pages/5. Exercise - Order of Operations.xcplaygroundpage/Contents.swift
@@ -5,15 +5,21 @@
  */
 
 
+print(20) //Quick ! guess
+print(10 + 2 * 5) // good guess
+
 //:  In a separate `print` statement, add in the necessary parentheses so that addition takes place before multiplication.
 
+print((10 + 2) * 5) //addition first
 
 //:  Print out what you think 4 * 9 - 6 / 2 evaluates to. Then print out the actual expression.
 
+print(33) // really quick guess
+print(4 * 9 - 6 / 2) // balling
 
 //:  In a separate `print` statement, add in the necessary parentheses so that the subtraction is prioritized over the multiplication and division.
 
-
+print(4 * (9 - 6) / 2) // 4 * 3 / 2
 /*:
 [Previous](@previous)  |  page 5 of 8  |  [Next: App Exercise - Complex Fitness Calculations](@next)
  */
