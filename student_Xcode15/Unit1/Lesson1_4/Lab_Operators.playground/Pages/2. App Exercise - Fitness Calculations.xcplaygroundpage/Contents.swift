290732ff068ad24626ec67336170c40e48e704e1
OmarEshiet <157065542+OmarEshiet@users.noreply.github.com>
2024-10-25 15:19:32 -0400

    Update Contents.swift

--- student_Xcode15/Unit1/Lesson1_4/Lab_Operators.playground/Pages/2. App Exercise - Fitness Calculations.xcplaygroundpage/Contents.swift
+++ student_Xcode15/Unit1/Lesson1_4/Lab_Operators.playground/Pages/2. App Exercise - Fitness Calculations.xcplaygroundpage/Contents.swift
@@ -7,12 +7,31 @@
  */
 
 
+let heartRate1 = 74
+let heartRate2 = 69
+let heartRate3 = 92
+let addedHR = heartRate1 + heartRate2 + heartRate3
+let averageHR = addedHR / 3
+print(averageHR)
+
+
+
 //:  Now create three more constants, `heartRate1D`, `heartRate2D`, and `heartRate3D`, equal to the same values as `heartRate1`, `heartRate2`, and `heartRate3`. These new constants should be of type `Double`. Create a constant `addedHRD` equal to the sum of all three heart rates. Create a constant called `averageHRD` that equals the `addedHRD` divided by 3 to get the average of your new heart rate constants. Print the result. Does this differ from your previous average? Why or why not?
 
+let heartRate1D : Double = 74
+let heartRate2D : Double = 69
+let heartRate3D : Double = 92
+let addedHRD = heartRate1D + heartRate2D + heartRate3D
+let averageHRD = addedHRD / 3
+print(averageHRD)
+print("When every number is a double there is now an added decimal point ni==instead of the output rounding down")
 
 //:  Imagine that partway through the day a user has taken 3,467 steps out of the 10,000 step goal. Create constants `steps` and `goal`. Both will need to be of type `Double` so that you can perform accurate calculations. `steps` should be assigned the value 3,467, and `goal` should be assigned 10,000. Create a constant `percentOfGoal` that equals an expression that evaluates to the percent of the goal that has been achieved so far.
 
-
+let steps : Double = 3467
+let goal : Double = 10000
+let percentOfGoal = (steps / goal) * 100 // equation to get percent of 10000
+ print(percentOfGoal)
 /*:
 [Previous](@previous)  |  page 2 of 8  |  [Next: Exercise - Compound Assignment](@next)
- */
+             */ 
