4d5088969b8e0f70164ba523f4819c81ade011f9
OmarEshiet <157065542+OmarEshiet@users.noreply.github.com>
2024-11-01 14:32:23 -0400

    first timeDebugging

--- student_Xcode15/Unit1/Lesson1_7/lab/FirstTimeDebugging/FirstTimeDebugging/ViewController.swift
+++ student_Xcode15/Unit1/Lesson1_7/lab/FirstTimeDebugging/FirstTimeDebugging/ViewController.swift
@@ -5,7 +5,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        var sample = "sample"
+        let sample = "sample"
         print(sample)
         
         if false {
