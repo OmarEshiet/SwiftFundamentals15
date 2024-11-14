83b8cd792716a604dab2e7dd2ff0c2dde8b4c9d0
OmarEshiet <157065542+OmarEshiet@users.noreply.github.com>
2024-11-14 14:45:17 -0500

    exercise with strings

--- student_Xcode15/Unit2/Lesson2_2/Lab_Strings.playground/Pages/2. Exercise - Concatenation and Interpolation.xcplaygroundpage/Contents.swift
+++ student_Xcode15/Unit2/Lesson2_2/Lab_Strings.playground/Pages/2. Exercise - Concatenation and Interpolation.xcplaygroundpage/Contents.swift
@@ -3,10 +3,13 @@
  
  Create a `city` constant and assign it a string literal representing your home city. Then create a `state` constant and assign it a string literal representing your home state. Finally, create a `home` constant and use string concatenation to assign it a string representing your home city and state (i.e. Portland, Oregon). Print the value of `home`.
  */
-
+let city = "Portland"
+let state = "Oregon"
+let home  = city + ", " + state
 
 //:  Use the compound assignment operator (`+=`) to add `home` to `introduction` below. Print the value of `introduction`.
 var introduction = "I live in "
+introduction += home
 
 
 /*:
@@ -16,7 +19,9 @@ var introduction = "I live in "
  
  Insert `name` where indicated, and insert a mathematical expression that evaluates to your current age plus one where indicated.
  */
-
+let name = "Omar Eshiet"
+let age = 21
+print("My name is " + name + " and after my next birthday I will be ", age,"years old.")
 
 /*:
 [Previous](@previous)  |  page 2 of 5  |  [Next: App Exercise - Notifications](@next)
