3a8a04280068afd5e12a0964bff6233e75250404
OmarEshiet <157065542+OmarEshiet@users.noreply.github.com>
2024-12-01 23:34:02 -0500

    calcLayout

---
+++ student_Xcode15/Unit2/Lesson2_9/calculatorTutorial/calculatorTutorial/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  calculatorTutorial
+//
+//  Created by remotestudent on 12/1/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var calcResults: UILabel!
+    
+    @IBOutlet weak var calcInfo: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func clearButton(_ sender: Any) {
+    }
+    
+    @IBAction func negPosButton(_ sender: Any) {
+    }
+    
+    @IBAction func divButton(_ sender: Any) {
+    }
+    
+    @IBAction func sevenButton(_ sender: Any) {
+    }
+    
+}
+
