872d24bb675cfcb02702831de90433be1a8c73e4
OmarEshiet <157065542+OmarEshiet@users.noreply.github.com>
2024-12-08 23:53:49 -0500

    applePie

---
+++ student_Xcode15/Unit2/Lesson2_9/Apple Pie/Apple Pie/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Apple Pie
+//
+//  Created by remotestudent on 12/8/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var TreeImageView: UIImageView!
+    
+    @IBOutlet weak var correctWordLabel: UILabel!
+    
+    @IBOutlet weak var scoreLabel: UILabel!
+    
+    @IBOutlet var letterButtons: [UIButton]!
+   
+    
+    @IBAction func buttonPressed(_ sender: UIButton) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
